22e4189b70559de8e31e1dfc527c26ee9efad858
Ren <renato.martins39@gmail.com>
2022-03-10 09:56:04 +0000

    Upload

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -3,12 +3,155 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// Converting units from a user input
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// Renato Martins version 0.1
     /// </author>
     public class DistanceConverter
     {
+        //
+        // Units!
+        //
+        public const double METRES_IN_MILES = 0.000621;
+        public const double FEET_IN_METRES = 3.28084;
+        public const int FEET_IN_MILES = 5280;
+
+        private double fromDistance;
+        private double toDistance;
+        private string fromUnit;
+        private string toUnit;
+
+        //
+        // Names!
+        //
+        public const string FEET = "Feet";
+        public const string METRES = "Metres";
+        public const string MILES = "Miles";
+
+        public DistanceConverter()
+        {
+            fromUnit = MILES;
+            toUnit = FEET;
+        }
+
+        public void ConvertDistance()
+        {
+            fromUnit = SelectUnit("Original Unit: ");
+
+            toUnit = SelectUnit("Unit to be calculated: ");
+
+            OutputHeading($"Converting {fromUnit} to {toUnit}");
+
+            fromDistance = InputDistance($"Number of {fromUnit}: ");
+
+            CalculateDistance();
+            OutputDistance();
+        }
+
+        private void CalculateDistance()
+        {
+            if (fromUnit == MILES && toUnit == FEET)
+            {
+                toDistance = fromDistance * FEET_IN_MILES;
+            }
+            else if (fromUnit == FEET && toUnit == MILES)
+            {
+                toDistance = fromDistance / FEET_IN_MILES;
+            }
+            else if (fromUnit == MILES && toUnit == METRES)
+            {
+                toDistance = fromDistance / METRES_IN_MILES;
+            }
+            else if (fromUnit == METRES && toUnit == MILES)
+            {
+                toDistance = fromDistance * METRES_IN_MILES;
+            }
+            else if (fromUnit == FEET && toUnit == METRES)
+            {
+                toDistance = fromDistance * FEET_IN_METRES;
+            }
+            else if (fromUnit == METRES && toUnit == FEET)
+            {
+                toDistance = fromDistance / METRES_IN_MILES;
+            }
+        }
+
+        //
+        // Show Choices on screen
+        //
+        private string SelectUnit(string prompt)
+        {
+            string choice = DisplayChoices(prompt);
+            return ExecuteChoice(choice);
+        }
+
+        private static string ExecuteChoice(string choice)
+        {
+
+            if (choice.Equals("1"))
+            {
+                return METRES;
+            }
+            else if (choice.Equals("2"))
+            {
+                return MILES;
+            }
+            else if (choice.Equals("3"))
+            {
+                return FEET;
+            }
+
+            return null;
+        }
+
+        //
+        //Console output display
+        //
+        private static string DisplayChoices(string prompt)
+        {
+            Console.WriteLine();
+            Console.WriteLine($" 1. {METRES}");
+            Console.WriteLine($" 2. {MILES}");
+            Console.WriteLine($" 3. {FEET}");
+            Console.WriteLine();
+
+            Console.Write(prompt);
+            string choice = Console.ReadLine();
+            return choice;
+        }
+
+
+        private double InputDistance(string prompt)
+        {
+            Console.Write(prompt);
+            string value = Console.ReadLine();
+            return Convert.ToDouble(value);
+        }
+
+        //
+        // Final Output with maths done
+        //
+        private void OutputDistance()
+        {
+            Console.WriteLine($" {fromDistance} {fromUnit}" +
+            $" is {toDistance} {toUnit}");
+        }
+
+        private void OutputHeading(String prompt)
+        {
+            Console.WriteLine("\n============================");
+            Console.WriteLine("      Distance Converter      ");
+            Console.WriteLine("        Renato Martins        ");
+            Console.WriteLine("============================\n");
+
+            Console.WriteLine(prompt);
+            Console.WriteLine("");
+        }
+
+
+
+
+
     }
 }
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -5,14 +5,6 @@ using System;
 
 namespace ConsoleAppProject
 {
-    /// <summary>
-    /// The main method in this class is called first
-    /// when the application is started.  It will be used
-    /// to start App01 to App05 for CO453 CW1
-    /// 
-    /// This Project has been modified by:
-    /// Derek Peacock 05/02/2022
-    /// </summary>
     public static class Program
     {
         public static void Main(string[] args)
@@ -22,12 +14,12 @@ namespace ConsoleAppProject
             Console.WriteLine();
             Console.WriteLine(" =================================================");
             Console.WriteLine("    BNU CO453 Applications Programming 2021-2022! ");
-            Console.WriteLine("        by Derek                                  ");
+            Console.WriteLine("        by Renato                                 ");
             Console.WriteLine(" =================================================");
             Console.WriteLine();
 
             DistanceConverter converter = new DistanceConverter();
-            converter.Run();
+            converter.ConvertDistance();
         }
     }
 }
